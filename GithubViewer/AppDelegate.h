57ddf2e7b8e32f6f35be7228bb3139724a606170
vlasdv <vlas.dv@gmail.com>
2014-11-11 15:15:27 +0300

    Initial Commit

---
+++ GithubViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GithubViewer
+//
+//  Created by Dmitrii Vlasov on 11.11.14.
+//  Copyright (c) 2014 Dmitry Vlasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GithubViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GithubViewer
+//
+//  Created by Dmitrii Vlasov on 11.11.14.
+//  Copyright (c) 2014 Dmitry Vlasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
